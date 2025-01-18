b2cb58218b881729a3c9f9c9a9ee92c3ac2bd183
Mavenry <sundaybukola10@gmail.com>
2025-01-18 20:16:03 +0100

    Project updated

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connectmongodb+srv://sundaybukola10:LnFR4rSJaLgwBswU@cluster0.yjspc.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0
+		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connecting to MONGODB", error.message);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
+	plugins: [react()],
+	server: {
 		proxy: {
 			"/api": {
 				target: "http://localhost:5000",
 			},
 		},
 	},
-})
+});
