909a1bee37eff036889e7f5b8c3bee4680786da9
Mavenry <sundaybukola10@gmail.com>
2025-01-22 22:00:11 +0100

    AnalyticsTab added

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import { Minus, Plus, Trash } from "lucide-react";
 import { useCartStore } from "../stores/useCartStore";
 
 const CartItem = ({ item }) => {
-	const { removeFromCart, updateQuantity } = useCartStore();
+	const { removeFromCart, updateQuantity } = useCartStore();.
 
 	return (
 		<div className='rounded-lg border p-4 shadow-sm border-gray-700 bg-gray-800 md:p-6'>
