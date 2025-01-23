6f54fb8901eef45a6146c31955075a4e37ac3a4d
Mavenry <sundaybukola10@gmail.com>
2025-01-23 22:19:18 +0100

    AnalyticsController updated

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -10,7 +10,7 @@ export const getAnalyticsData = async () => {
 			$group: {
 				_id: null, // it groups all documents together,
 				totalSales: { $sum: 1 },
-				totalRevenue: { $sum: "$totalAmount" },.
+				totalRevenue: { $sum: "$totalAmount" },
 			},
 		},
 	]);
