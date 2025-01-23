e564e7b018a57fa435a4c823ae5f9d4fb2f21a13
Mavenry <sundaybukola10@gmail.com>
2025-01-23 21:51:02 +0100

    analyticscontroller added

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -5,7 +5,6 @@ import User from "../models/user.model.js";
 export const getAnalyticsData = async () => {
 	const totalUsers = await User.countDocuments();
 	const totalProducts = await Product.countDocuments();
-
 	const salesData = await Order.aggregate([
 		{
 			$group: {
