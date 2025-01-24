01db86bc82288a501f74f0e1e54b7aaf5d414cb2
Mavenry <sundaybukola10@gmail.com>
2025-01-24 05:58:20 +0100

    analytics route updated

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,7 +4,7 @@ import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, adminRoute, async (req, res) => {
+router.get("/", protectRoute, adminRoute, async (req, res) => {.
 	try {
 		const analyticsData = await getAnalyticsData();
 
