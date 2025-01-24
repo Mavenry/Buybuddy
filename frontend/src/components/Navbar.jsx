9afb2cf052a60d7b346ca1dd1043eedf089c65a7
Mavenry <sundaybukola10@gmail.com>
2025-01-24 07:10:15 +0100

    Navigation Bars added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useUserStore } from "../stores/useUserStore";
 
 const Navbar = () => {
 	const { user, logout } = useUserStore();
-	const isAdmin = user?.role === "admin";.
+	const isAdmin = user?.role === "admin";
 	const { cart } = useCartStore();
 
 	return (
